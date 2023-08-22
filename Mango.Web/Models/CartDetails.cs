7fa9aa41d65a7e6521c10b8c02db99234534863d
Yahya KARAYEL <karayelyahya@gmail.com>
2023-08-22 22:21:26 +0300

    Add Cart Service to Web Project

---
+++ Mango.Web/Models/CartDetails.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Http.Headers;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+	{
+		[Key]
+        public int CartDetailsId { get; set; }
+		[ForeignKey("CartHeaderId")]
+        public int CartHeaderId { get; set; }
+        public CartHeader CartHeader { get; set; }
+        public int ProductId { get; set; }
+        [NotMapped]
+        public ProductDto Product { get; set; }
+        public int Count { get; set; }
+
+
+
+
+
+    }
+}
